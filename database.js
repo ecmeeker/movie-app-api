0d5e793cb494a317f37e62933b83f2235037a607
Cassandra <guest1@Cassandras-MacBook-Pro.local>
2022-07-24 19:38:22 -0500

    Initial commit

---
+++ database.js
@@ -0,0 +1,34 @@
+var sqlite3 = require('sqlite3').verbose();
+var md5 = require('md5');
+
+const DBSOURCE = "db.sqlite";
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    //DB error;
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log('Connected to the SQLite database!');
+    db.run(`CREATE TABLE movie (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name text,
+      genre text,
+      platform text,
+      description text UNIQUE,
+      watched boolean,
+      dateWatched Date,
+      CONSTRAINT description_unique UNIQUE (description)
+    )`,
+    (err) => {
+      if (err){
+
+      } else {
+        //Table created
+        console.log('Movie table created!');
+      }
+    });
+  }
+});
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,138 @@
+// Create app
+var express = require("express");
+var app = express();
+var db = require("./database.js");
+var cors = require("cors")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Server port
+var HTTP_PORT = 8000;
+// Start Server
+app.listen(HTTP_PORT, () => {
+  console.log('Server running on port %HTTP_PORT%'.replace('%HTTP_PORT%',HTTP_PORT));
+});
+
+// Root endpoint
+app.get('/', (req, res, next) => {
+  res.json({'message':'Ok'});
+});
+
+// CRUD API Enpoints
+// GET /movies
+app.get("/movies", (req, res, next) => {
+  var sql = "select * from movie";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err){
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// GET /movie/:id
+app.get("/movie/:id", (req, res, next) => {
+  var sql = "select * from movie where id = ?";
+  var params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({"error": err.message});
+      return;
+    }
+    res.json(row);
+  });
+});
+
+// POST /movie/
+app.post("/movie/", (req, res, next) => {
+  var errors = [];
+  var data = {
+    name: req.body.name,
+    genre: req.body.genre,
+    platform: req.body.platform,
+    description: req.body.description,
+    watched: req.body.watched,
+    dateWatched: req.body.dateWatched
+  }
+  db.run(`INSERT INTO movie (name, genre, platform, description, watched, dateWatched) VALUES (?,?,?,?,?,?)`,[data.name, data.genre, data.platform, data.description, "false", data.dateWatched],
+  (err, result) => {
+    if (err){
+      res.status(400).json({"error": res.message});
+      return;
+    }
+    res.json(data);
+  });
+});
+
+// GET /lastinsert
+app.get("/movie/lastinsert", (req, res, next) => {
+  var sql = "select last_insert_rowid()";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err){
+      res.status(400).json({"error":err.message});
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+// PATCH /movie/:id
+app.patch("/movie/:id", (req, res, next) => {
+  var data = {
+    name: req.body.name,
+    genre: req.body.genre,
+    platform: req.body.platform,
+    description: req.body.description,
+    watched: req.body.watched,
+    dateWatched: req.body.dateWatched
+  }
+  db.run(
+    `UPDATE movie set
+      name = COALESCE(?,name),
+      genre = COALESCE(?,genre),
+      platform = COALESCE(?,platform),
+      description = COALESCE(?,description),
+      watched = COALESCE(?,watched),
+      dateWatched = COALESCE(?,dateWatched)
+      WHERE id = ?`,
+    [data.name, data.genre, data.platform, data.description, data.watched, data.dateWatched, req.params.id],
+    (err, result) => {
+      if (err){
+        res.status(400).json({"error": res.message});
+        return;
+      }
+      res.json({
+        message: "success",
+        data: data,
+        changes: this.changes
+      });
+    });
+});
+
+// DELETE /movie/:id
+app.delete("/movie/:id", (req, res, next) => {
+  db.run(
+    `DELETE FROM movie WHERE id = ?`,
+    req.params.id,
+    (err, result) => {
+      if (err) {
+        res.status(400).json({"error": err.message});
+        return;
+      }
+    });
+    res.json({
+      message:"deleted",
+      changes: this.changes
+    });
+  });
+
+// Default response
+app.use((req, res) => {
+  res.status(404);
+});
